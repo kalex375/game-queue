9aeb71df49f59182f8e8a95ed12b922e2b183204
DanMir0 <95764144+DanMir0@users.noreply.github.com>
2022-09-14 21:43:15 +0300

    add registration page

---
+++ src/pages/RegistrationPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <RegistrationForm></RegistrationForm>
+        <p>Already have an anccount? <a>Sign In</a></p>
+    </div>
+</template>
+
+<script setup>
+import RegistrationForm from '@/components/RegistrationForm.vue';
+
+</script>
+
+<style scoped>
+
+</style>
