bd19033b8b52ffa9979cbd28f41aab112396481f
rexx0n <alexey.mironenko46@gmail.com>
2022-09-17 13:48:59 +0300

    complete checkbox

--- src/components/UI/GqButton.vue
+++ src/components/UI/GqButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="GqButton">
+  <button class="btn">
     <slot></slot>
   </button>
 </template>
@@ -8,24 +8,5 @@
 
 <style scoped lang="scss">
 @import "@/assets/variables.scss";
-.GqButton {
-  transition: 200ms;
-  font-family: "Nunito Sans", Sans-serif;
-  font-size: 14px;
-  font-weight: 700;
-  text-transform: uppercase;
-  font-style: normal;
-  line-height: 25px;
-  letter-spacing: 0.8px;
-  padding: 15px 50px;
-  background: $bg-button;
-  color: #eeeeee;
-  border: none;
-  margin-bottom: 5px;
-  border-bottom: $border-cl-btn solid 5px;
-  &:hover {
-    background: $border-cl-btn;
-    border-color: $bg-button;
-  }
-}
+
 </style>
--- src/components/UI/GqCheckbox.vue
+++ src/components/UI/GqCheckbox.vue
@@ -1,5 +1,19 @@
 <template>
-  <input type="checkbox" id="checkbox" v-model="checked" />
+  <div class="checkbox">
+    <input type="checkbox" id="checkbox" v-model="checked" />
+    <label for="">Remeber me</label>
+  </div>
+  <!-- не нужный код ↑↑↑ (если я удаляю то вылетает ошибки не знаю как их решать)  -->
+  <div>
+    <input
+      id="highload0"
+      class="checkbox-custom"
+      name="highload0"
+      type="checkbox"
+    />
+    <label for="highload0" class="checkbox-custom-label">Remeber me</label>
+  </div>
+  <div>sdasdasasdasdassdada ad</div>
 </template>
 
 <script setup>
@@ -24,4 +38,29 @@ const checked = computed({
 
 <style scoped lang="scss">
 @import "@/assets/variables.scss";
+#highload0 {
+  display: none;
+}
+#highload0 ~ label::before #highload0 {
+  display: none;
+}
+#highload0 ~ label::before {
+  content: "✓";
+  text-align: center;
+  color: #222831;
+  line-height: 1em;
+  width: 1em;
+  height: 1em;
+  border: 2px inset white;
+  background: #222831;
+  border-radius: 0.25em;
+  margin: 0.25em;
+  display: inline-block;
+}
+#highload0:checked ~ label::before {
+  content: "✓";
+  color: white;
+  border: 2px inset rgb(255, 255, 255);
+  background: #222831;
+}
 </style>
