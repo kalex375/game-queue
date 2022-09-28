f1e388a5a72364d04dc3bd73cf563a41d9cd64dd
rexx0n <alexey.mironenko46@gmail.com>
2022-09-28 19:24:55 +0300

    make NotFound page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,28 @@
+<template>
+    <GqContainer>
+        <div>
+            <h1 class="mt-5">Sorry, this page does not exist</h1>
+            <router-link :to="{name: 'sign-in'}"
+                ><GqButton>Back to home</GqButton></router-link
+            >
+        </div>
+    </GqContainer>
+    <!-- или на dashboard-->
+</template>
+
+<script setup>
+import GqContainer from '@/components/UI/GqContainer.vue'
+import GqButton from '@/components/UI/GqButton.vue'
+</script>
+
+<style lang="scss" scoped>
+h1 {
+    text-align: center;
+}
+div {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 1rem;
+}
+</style>
