5a8851839297548995e61a3dc522738ec070b089
rexx0n <alexey.mironenko46@gmail.com>
2022-09-30 17:25:17 +0300

    create header

---
+++ src/components/UI/GqHeader.vue
@@ -0,0 +1,55 @@
+<template>
+    <header class="p-4">
+        <h1>Game-queue</h1>
+        <div class="items">
+            <div class="user">
+                <a href="profile"
+                    ><img
+                        src="https://www.klerk.ru/images/noavatar.webp"
+                        alt="avatar"
+                /></a>
+                <p>username</p>
+            </div>
+            <GqButton>Logout</GqButton>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/variables.scss';
+header {
+    display: flex;
+    justify-content: space-between;
+    background-image: url('https://templatekit.tokokoo.com/knight/wp-content/uploads/sites/17/2021/01/navbar-bg.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    align-items: center;
+    border-bottom: 2px solid $color_secondary;
+}
+.user {
+    flex-direction: column;
+}
+h1 {
+    font-family: $font_secondary;
+    font-size: 50px;
+}
+div {
+    display: flex;
+    align-items: center;
+}
+p {
+    font-family: $font_secondary;
+}
+img {
+    width: 35px;
+    height: 35px;
+    border-radius: 20px;
+}
+.items {
+    gap: 2rem;
+}
+</style>
--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,4 +1,5 @@
 <template>
+    <GqHeader></GqHeader>
     <div>
         {{ user.email }}
         <GqContainer class="m-auto">
